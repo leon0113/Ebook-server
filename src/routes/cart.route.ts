fdf027d37c3891588bb96ba85113261baa139a52
Leon <tahjib.leon399@gmail.com>
2024-11-06 19:26:42 +0600

    clear cart items

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,4 +1,4 @@
-import { getCart, updateCart } from "@/controllers/cart.controller";
+import { clearCart, getCart, updateCart } from "@/controllers/cart.controller";
 import { isAuth } from "@/middlewares/isAuth.middleware";
 import { cartItemSchema, validate } from "@/middlewares/validator.middleware";
 import { Router } from "express";
@@ -8,5 +8,6 @@ const cartRouter = Router();
 
 cartRouter.post('/', isAuth, validate(cartItemSchema), updateCart);
 cartRouter.get('/', isAuth, getCart);
+cartRouter.post('/clear', isAuth, clearCart);
 
 export default cartRouter;
