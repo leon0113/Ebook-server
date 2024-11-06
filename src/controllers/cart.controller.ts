c3d23946b757baa5a3b5c1fd345258001213aea1
Leon <tahjib.leon399@gmail.com>
2024-11-06 19:23:20 +0600

    get cart items

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,6 +1,90 @@
+import { ObjectId } from 'mongoose';
+import CartModel from "@/models/cart.model";
 import { cartItemsReqHandler } from "@/types";
 import { RequestHandler } from "express";
+import { sendErrorResponse } from '@/utils/helper';
+
+interface getCartDetails {
+    items: {
+        product: {
+            _id: ObjectId;
+            title: string;
+            cover?: {
+                url: string;
+                id: string
+            };
+            price: {
+                mrp: number;
+                sale: number
+            };
+            slug: string;
+        };
+        quantity: number;
+    }[]
+}
+
 
 export const updateCart: RequestHandler<{}, {}, cartItemsReqHandler> = async (req, res) => {
+    const { items } = req.body;
 
+    let cart = await CartModel.findOne({ userId: req.user.id });
+
+    if (!cart) {
+        cart = await CartModel.create({ userId: req.user.id, items });
+    } else {
+        for (const item of items) {
+            const oldProduct = cart.items.find((p) => item.product = p.product.toString());
+            if (oldProduct) {
+                oldProduct.quantity += item.quantity;
+                // if the quantity is 0 or less then remove the product from the cart
+                if (oldProduct.quantity <= 0) {
+                    cart.items = cart.items.filter(({ product }) => oldProduct.product !== product);
+                }
+            } else {
+                cart.items.push({
+                    product: item.product as any,
+                    quantity: item.quantity
+                });
+            }
+        }
+    };
+
+    await cart.save();
+
+    res.json({ cart: cart._id })
+};
+
+
+export const getCart: RequestHandler = async (req, res) => {
+    const cart = await CartModel.findOne({ userId: req.user.id }).populate<getCartDetails>({
+        path: "items.product",
+        select: "title cover slug price"
+    });
+
+    if (!cart) {
+        return sendErrorResponse({
+            res,
+            status: 404,
+            message: "Cart not found",
+        })
+    }
+
+    res.json({
+        cart: {
+            id: cart._id,
+            items: cart.items.map((item) => ({
+                product: {
+                    id: item.product._id,
+                    title: item.product.title,
+                    cover: item.product.cover,
+                    slug: item.product.slug,
+                    price: {
+                        mrp: (item.product.price.mrp / 100).toFixed(2),
+                        sale: (item.product.price.sale / 100).toFixed(2)
+                    }
+                },
+                quantity: item.quantity
+            }))
+        }
+    })
 }
--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,4 +1,4 @@
-import { updateCart } from "@/controllers/cart.controller";
+import { getCart, updateCart } from "@/controllers/cart.controller";
 import { isAuth } from "@/middlewares/isAuth.middleware";
 import { cartItemSchema, validate } from "@/middlewares/validator.middleware";
 import { Router } from "express";
@@ -6,6 +6,7 @@ import { Router } from "express";
 const cartRouter = Router();
 
 
-cartRouter.post('/', isAuth, validate(cartItemSchema), updateCart)
+cartRouter.post('/', isAuth, validate(cartItemSchema), updateCart);
+cartRouter.get('/', isAuth, getCart);
 
 export default cartRouter;
