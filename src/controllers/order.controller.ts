f969df9c08c1282bd769db0f1b6881fd9bf2f3c2
Leon <tahjib.leon399@gmail.com>
2024-11-10 19:25:27 +0600

    check order status

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,6 +1,8 @@
 import { BookDoc } from "@/models/books.model";
 import OrderModel from "@/models/order.model";
+import UserModel from "@/models/user.model";
 import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
 
 export const getOrders: RequestHandler = async (req, res) => {
     const orders = await OrderModel.find({ userId: req.user.id }).populate<{
@@ -34,4 +36,22 @@ export const getOrders: RequestHandler = async (req, res) => {
             }
         })
     })
+};
+
+
+export const getOrderStatus: RequestHandler = async (req, res) => {
+    const { bookId } = req.params;
+    let status = "false";
+
+    if (!isValidObjectId(bookId)) {
+        res.json({ status });
+        return;
+    }
+
+    const user = await UserModel.findOne({ _id: req.user.id, books: bookId });
+
+    if (user) status = "true";
+
+    res.json({ status });
 }
+
--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,9 +1,10 @@
-import { getOrders } from "@/controllers/order.controller";
+import { getOrders, getOrderStatus } from "@/controllers/order.controller";
 import { isAuth } from "@/middlewares/isAuth.middleware";
 import { Router } from "express";
 
 const orderRouter = Router();
 
-orderRouter.get('/', isAuth, getOrders)
+orderRouter.get('/', isAuth, getOrders);
+orderRouter.get('/check-status/:bookId', isAuth, getOrderStatus);
 
 export default orderRouter;
